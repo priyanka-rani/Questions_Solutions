6c0aa36b2da8747a3979986e99ea37f9f9dfc2ce
priyanka-rani <priyanka-rani@users.noreply.github.com>
2020-01-16 14:57:51 +0600

    Update Answer_2b.java

--- Answer_2b.java
+++ Answer_2b.java
@@ -1,47 +1,43 @@
-package com.securepenny.cashbaba;
 
-import java.util.Arrays;
+/*I used abstract class Vehicle instead of interface. I know it is the same as using an interface
+but still it's a different design patterns  */
+abstract class Vehicle {
+    abstract int set_num_of_wheels() ;
+    abstract int set_num_of_passengers() ;
+    abstract boolean has_gas() ;
+}
+class Car extends Vehicle{
 
-public class Answer_2a {
-    /*I used abstract class Vehicle instead of interface. I know it is the same as using an interface
-    but still it's a different design patterns  */
-    abstract class Vehicle {
-        abstract int set_num_of_wheels() ;
-        abstract int set_num_of_passengers() ;
-        abstract boolean has_gas() ;
+    @Override
+    public int set_num_of_wheels() {
+        return 4;
     }
-    class Car extends Vehicle{
 
-        @Override
-        public int set_num_of_wheels() {
-            return 4;
-        }
-
-        @Override
-        public int set_num_of_passengers() {
-            return 4;
-        }
-
-        @Override
-        public boolean has_gas() {
-            return true;
-        }
+    @Override
+    public int set_num_of_passengers() {
+        return 4;
     }
-    class Plane extends Vehicle{
 
-        @Override
-        public int set_num_of_wheels() {
-            return 14;
-        }
+    @Override
+    public boolean has_gas() {
+        return true;
+    }
+}
+class Plane extends Vehicle{
+//number of wheels depends on plane model and size.
+    @Override
+    public int set_num_of_wheels() {
+        return 14;
+    }
+//number of passengers depends on plane model and size.
+    @Override
+    public int set_num_of_passengers() {
+        return 500;
+    }
 
-        @Override
-        public int set_num_of_passengers() {
-            return 500;
-        }
-
-        @Override
-        public boolean has_gas() {
-            return false;
-        }
+    @Override
+    public boolean has_gas() {
+        //as plane uses jet fuel which is liquid I guess so the answer is false
+        return false;
     }
 }
