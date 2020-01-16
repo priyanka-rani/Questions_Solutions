7b8a636f5f91c860a56a5b17f7519a1b0dd1019a
priyanka-rani <priyanka-rani@users.noreply.github.com>
2020-01-16 14:55:22 +0600

    Update Answer_1.java

--- Answer_1.java
+++ Answer_1.java
@@ -1,19 +1,14 @@
-package com.securepenny.cashbaba;
 
-import java.util.Arrays;
+boolean checkAnagram(String string1, String string2) {
+    if (string1 == null || string2 == null) return false;
 
-public class Anagram {
-    boolean checkAnagram(String string1, String string2) {
-        if (string1 == null || string2 == null) return false;
-        
-         //Sort both String and check if they are equal
-        return sortString(string1).equals(string2);
-    }
-    
-    //Sort A String
-    String sortString(String str) {
-        char[] arr = str.toLowerCase().toCharArray();
-        Arrays.sort(arr);
-        return String.valueOf(arr);
-    }
+     //Sort both String and check if they are equal
+    return sortString(string1).equals(string2);
+}
+
+//Sort A String
+String sortString(String str) {
+    char[] arr = str.toLowerCase().toCharArray();
+    Arrays.sort(arr);
+    return String.valueOf(arr);
 }
