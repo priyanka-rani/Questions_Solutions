aade1ffdde45393a95de551eeaf72a909c99c751
priyanka-rani <priyanka-rani@users.noreply.github.com>
2020-01-16 14:44:58 +0600

    Update Answer_2a.java

--- Answer_2a.java
+++ Answer_2a.java
@@ -1,45 +1,40 @@
-package com.securepenny.cashbaba;
 
-import java.util.Arrays;
+interface Vehicle {
+    int set_num_of_wheels() ;
+    int set_num_of_passengers() ;
+    boolean has_gas() ;
+}
+class Car implements Vehicle{
 
-public class Answer_1 {
-    interface Vehicle {
-        int set_num_of_wheels() ;
-        int set_num_of_passengers() ;
-        boolean has_gas() ;
+    @Override
+    public int set_num_of_wheels() {
+        return 4;
     }
-    class Car implements Vehicle{
 
-        @Override
-        public int set_num_of_wheels() {
-            return 4;
-        }
-
-        @Override
-        public int set_num_of_passengers() {
-            return 4;
-        }
-
-        @Override
-        public boolean has_gas() {
-            return true;
-        }
+    @Override
+    public int set_num_of_passengers() {
+        return 4;
     }
-    class Plane implements Vehicle{
 
-        @Override
-        public int set_num_of_wheels() {
-            return 14;
-        }
+    @Override
+    public boolean has_gas() {
+        return true;
+    }
+}
+class Plane implements Vehicle{
+// Number of wheels depends on Plane model and size
+    @Override
+    public int set_num_of_wheels() {
+        return 14;
+    }
+// Number of passengers depends on Plane model and size
+    @Override
+    public int set_num_of_passengers() {
+        return 500;
+    }
 
-        @Override
-        public int set_num_of_passengers() {
-            return 500;
-        }
-
-        @Override
-        public boolean has_gas() {
-            return false;
-        }
+    @Override
+    public boolean has_gas() {
+        return false;
     }
 }
