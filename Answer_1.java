3e01492b5795de04402fd46e0cf750af04cafa29
priyanka-rani <priyanka-rani@users.noreply.github.com>
2020-01-16 14:27:22 +0600

    Update Answer_1.java

--- Answer_1.java
+++ Answer_1.java
@@ -5,9 +5,12 @@ import java.util.Arrays;
 public class Anagram {
     boolean checkAnagram(String string1, String string2) {
         if (string1 == null || string2 == null) return false;
+        
+         //Sort both String and check if they are equal
         return sortString(string1).equals(string2);
     }
-
+    
+    //Sort A String
     String sortString(String str) {
         char[] arr = str.toLowerCase().toCharArray();
         Arrays.sort(arr);
